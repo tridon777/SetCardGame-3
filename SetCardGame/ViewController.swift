abf52eae40b2cbbaa3454bd1e95c2fbce4bfbd20
Paul Searcy <tridon777@Pauls-Mac-mini.local>
2015-02-14 16:15:03 -0600

    Initial Commit

---
+++ SetCardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SetCardGame
+//
+//  Created by Paul Searcy on 2/14/15.
+//  Copyright (c) 2015 Paul Searcy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
